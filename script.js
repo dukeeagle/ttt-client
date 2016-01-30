30ab68f65b03beb6e057cda7a04799f390a4c688
Luke Igel <luke.igel@gmail.com>
2016-01-30 15:50:51 -0600

    Makes canvas semi-scalable and further integrates socket.io

--- script.js
+++ script.js
@@ -1,19 +1,69 @@
-var app = require('express')();
-var http = require('http').Server(app);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+//CANVAS 
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
+function resize_canvas(){
+            canvas = document.getElementById("myCanvas");
+            if (canvas.width  < window.innerWidth)
+            {
+                canvas.width  = window.innerWidth;
+            }
+
+            if (canvas.height < window.innerHeight)
+            {
+                canvas.height = window.innerHeight;
+            }
+        }
 
 
-io.on('connection', function(socket){
-  console.log('a user connected');
-});
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
+
+
+
+
+
+
+/*var canvas;
+var canvasWidth;
+var ctx;
+
+	canvas = getElementById('myCanvas');
+	if(canvas.getContext){
+		ctx = canvas.getContext("2d");
+
+		window.addEventListener('resize', resizeCanvas, false);
+		window.addEventListener('orientationchange', resizeCanvas, false);
+		resizeCanvas()
+	}
+}
+
+function resizeCanvas(){
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+}
+
+var canvas = document.getElementById('mycanvas');
+var ctx = canvas.getContext('2d');
+
+//Copy canvas as image data
+var imgData = ctx.getImageData(0,0, canvas.width, canvas.height);
+
+//Resize original canvas
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+
+//Copy back to resized canvas
+//ctx.putImageData(imgData, 0, 0);
+/*var bgReady = false;
+var bgImage = new Image();
+bgImage.onload = function () {
+	bgReady = true;
+};
+
+
+
+      var canvas = document.getElementById("myCanvas");
+      var ctx = canvas.getContext("2d");
+      ctx.fillStyle = "#FF0000";
+      ctx.fillRect(0,0,150,75);
+*/
