8d68157a2549f914455f8142911f8cfd2529e21b
Luke <luke.igel@gmail.com>
2016-02-08 15:15:31 -0600

    Adds some test socket.io stuff

---
+++ index.js
@@ -0,0 +1,30 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var clients = {};
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', function (socket){
+	socket.on('add-user', function(data){
+		clients[data.username] = {
+			"socket":socket.id
+		};
+	});
+
+    socket.on('disconnect', function(){
+	for(var name in clients){
+			if(clients[name].socket === socket.id){
+					
+					delete clients[name];
+					break;
+			}
+	}
+	})
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
