350452b21f31bf32a915fb790d2a82a43466a86f
Luke Igel <luke.igel@gmail.com>
2016-03-08 22:36:46 -0600

    Successfully integrates Socket.IO!

--- ttt/www/js/services.js
+++ ttt/www/js/services.js
@@ -7,16 +7,29 @@ angular.module('starter')
 	return{
 		user:user
 	}
-});
+})
 
-/*.factory('socket', function(socketFactory)){
-	var myIoSocket = io.connect('https://fathomless-brushlands-33586.herokuapp.com/');
-
-	mySocket = socketFactory({
-		ioSocket: myIoSocket
-	});
-
-	return mySocket;
-<<<<<<< HEAD
-}*/
-
+	
+.factory('socket', ['$rootScope', function ($rootScope) {
+  var socket = io.connect();
+  return {
+    on: function (eventName, callback) {
+      socket.on(eventName, function () {  
+        var args = arguments;
+        $rootScope.$apply(function () {
+          callback.apply(socket, args);
+        });
+      });
+    },
+    emit: function (eventName, data, callback) {
+      socket.emit(eventName, data, function () {
+        var args = arguments;
+        $rootScope.$apply(function () {
+          if (callback) {
+            callback.apply(socket, args);
+          }
+        });
+      })
+    }
+  };
+}]);
